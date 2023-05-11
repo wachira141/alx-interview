7c128ce721f6913399d67c8e2e60e6bf66b62b97
wachira140 <wachiraw424@gmail>
2023-05-11 20:20:17 +0300

    init from jacob git

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,15 +1,63 @@
 #!/usr/bin/python3
-'''
-function to check logs from NGINX
+'''A script for parsing HTTP request logs.
 '''
 import re
 
 
-def get_log_input():
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
     '''
-    get inputs from stin
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
+
+
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
     '''
-    status_code_list = {
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
+
+
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
         '200': 0,
         '301': 0,
         '400': 0,
@@ -17,80 +65,22 @@ def get_log_input():
         '403': 0,
         '404': 0,
         '405': 0,
-        '500': 0
-        }
-    file_size = 0
-    incrementer = 1
+        '500': 0,
+    }
     try:
         while True:
-            log_input = input()
-            regex_data = regex_parser(log_input)
-            count_codes = counter(regex_data['status'])
-            file_size += regex_data['file_size']
-
-            if count_codes in status_code_list.keys():
-                status_code_list[count_codes] += 1
-
-            if incrementer % 10 == 0:
-                print_result(status_code_list, file_size)
-            incrementer += 1
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
-        print_result(status_code_list, file_size)
-
-
-def counter(status_code):
-    '''get the number of replied status code
-    '''
-    status_list = [
-        '200',
-        '301',
-        '400',
-        '401',
-        '403',
-        '404',
-        '405',
-        '500'
-        ]
-    if status_code in status_list:
-        return status_code
-
-
-def print_result(status_code_list, file_size):
-    '''
-    print the status code list
-    '''
-    for k, v in status_code_list.items():
-        print('{}: {}'.format(k, v))
-    print('File size: {}'.format(file_size))
-
-
-def regex_parser(log_input):
-    '''
-    check if the line of log parsed
-    '''
-    rex = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<method>[^"]*)"\s*',
-        r'\s*(?P<status_code>\d+)',
-        r'\s*(?P<file_size>\d+)')
-
-    formated_log = '{} - {}{}{}{}\\s*'\
-        .format(rex[0], rex[1], rex[2], rex[3], rex[4])
-    resp_match = re.fullmatch(formated_log, log_input)
-
-    data = {
-        'status': 0,
-        'file_size': 0
-     }
-    if resp_match is not None:
-        status = resp_match.group('status_code')
-        file_size = resp_match.group('file_size')
-        data['status'] = status
-        data['file_size'] = int(file_size)
-
-    return data
+        print_statistics(total_file_size, status_codes_stats)
 
 
 if __name__ == '__main__':
-    get_log_input()
+    run()
