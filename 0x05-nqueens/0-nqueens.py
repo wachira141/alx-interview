7b3346a26311d5b264dca9f921c244edcfda6a27
wachira140 <wachiraw424@gmail>
2023-05-25 19:47:44 +0300

    n-queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+import sys
+
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    sys.exit(1)
+
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    sys.exit(1)
+
+board = [[0]*N for _ in range(N)]
+
+
+def is_attack(i, j):
+    # checking if there is a queen in row or column
+    for k in range(0, N):
+        if board[i][k] == 1 or board[k][j] == 1:
+            return True
+    # checking diagonals
+    for k in range(0, N):
+        for l in range(0, N):
+            if (k+l == i+j) or (k-l == i-j):
+                if board[k][l] == 1:
+                    return True
+    return False
+
+
+def N_queen(n):
+    # if n is 0, solution found
+    if n == 0:
+        return True
+    for i in range(0, N):
+        for j in range(0, N):
+            '''checking if we can place a queen here or not
+            queen will not be placed if the place is being attacked
+            or already occupied'''
+            if (not(is_attack(i, j))) and (board[i][j] != 1):
+                board[i][j] = 1
+                # recursion
+                # wether we can put the next queen with this arrangment or not
+                if N_queen(n-1) is True:
+                    return True
+                board[i][j] = 0
+
+    if not isinstance(N, int):
+        print('N must be a number')
+        sys.exit(1)
+    if N < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+
+    return False
+
+
+N_queen(N)
+for i in board:
+    print(i)
