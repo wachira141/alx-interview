01c43d686590b79e920fa8d89f083b22618fbb1e
wachira140 <wachiraw424@gmail>
2023-06-15 19:04:10 +0300

    change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from typing import List
+
+def making_change(arr: List, amount: int) -> int:
+    total = arr[len(arr) -1]
+    index = len(arr) -1
+    count = 0
+
+    result = rec_change(sorted(arr), total, amount,index, count)
+    print(result)
+
+
+
+def rec_change(arr, total, amount,index, count):
+    if index < 0:
+        return -1
+
+    count +=1
+    if total == amount:
+        return count
+
+    total = total + arr[index]
+
+    if total > amount:
+        total = total - arr[index]
+        if total > amount:
+            total = arr[index -1]
+        count -=1
+        return rec_change(arr, total, amount,index-1, count)
+    else:
+        return rec_change(arr, total, amount,index, count)
+
+
+making_change([1256, 54, 48, 16, 102], 1453)
+    
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,16 @@
+## Tasks
+<h4>Change comes from within</h4>
+<ul>
+Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+
+<li>Prototype: def makeChange(coins, total)</li>
+<li>Return: fewest number of coins needed to meet total
+<ul>
+    <li>If total is 0 or less, return 0</li>
+    <li>If total cannot be met by any number of coins you have, return -1</li>
+</ul>
+</li>
+<li>coins is a list of the values of the coins in your possession</li>
+<li>The value of a coin will always be an integer greater than 0</li>
+<li>You can assume you have an infinite number of each denomination of coin in the list</li>
+<li>Your solution’s runtime will be evaluated in this task</li>
